49e20f818604eac9d638ea48154dd2ca54795107
Alina Malginova <39488063+AlinaMalginova1998@users.noreply.github.com>
2022-06-25 16:53:50 +0300

    rebuild popups

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// --------- buttons ---------
+// кнопки страницы
 const editButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 
-// ---------- elements ---------
-const main = document.querySelector(".main");
-let cardsContainer = document.querySelector(".cards");
-let nameElement = document.querySelector(".profile__name");
-let descriptionElement = document.querySelector(".profile__description");
+// элементы страницы
+const cardsContainer = document.querySelector(".cards");
+const nameElement = document.querySelector(".profile__name");
+const descriptionElement = document.querySelector(".profile__description");
 
 // --------- данные исходных карточек -----------
 const initialCards = [
@@ -39,116 +38,86 @@ const initialCards = [
 // ----------------------- РЕДАКТИРОВАНИЕ ДАННЫХ -------------------------------
 
 function editProfileInfo() {
-  const popupTemplate = document.querySelector("#popup").content;
-  const editProfilePopupElement = popupTemplate
-    .querySelector(".popup")
-    .cloneNode(true);
-  const exitPopupButton = editProfilePopupElement.querySelector(
-    ".popup__exit-button"
-  );
-  const saveButton = editProfilePopupElement.querySelector(
-    ".popup__save-button"
-  );
-  const formElement = editProfilePopupElement.querySelector(
-    ".popup__form-container"
-  );
-
-  // настраиваем элементы модального окна
-  editProfilePopupElement.querySelector(".popup__title").textContent =
-    "Редактировать профиль";
-  saveButton.textContent = "Сохранить";
-  let nameInput = editProfilePopupElement.querySelector(
-    ".popup__form-container"
-  ).firstElementChild;
-  let descriptionInput = nameInput.nextElementSibling;
-
-  nameInput.classList.add("popup__input_el_name");
-  nameInput.setAttribute("name", "name");
-  nameInput.setAttribute("placeholder", "Введите свое имя");
-
-  descriptionInput.classList.add("popup__input_el_description");
-  descriptionInput.setAttribute("name", "description");
-  descriptionInput.setAttribute("placeholder", "Напишите немного о себе");
+  const popup = document.querySelector(".popup_type_edit-profile");
+  const exitPopupButton = popup.querySelector(".popup__exit-button");
+  const saveButton = popup.querySelector(".popup__save-button");
+  const formElement = popup.querySelector(".popup__form-container");
 
   // заполняем инпуты данными профиля
-  editProfilePopupElement.querySelector(".popup__input_el_name").value =
-    nameElement.textContent;
-  editProfilePopupElement.querySelector(".popup__input_el_description").value =
+  popup.querySelector(".popup__input_el_name").value = nameElement.textContent;
+  popup.querySelector(".popup__input_el_description").value =
     descriptionElement.textContent;
 
   // открываем окно
-  editProfilePopupElement.classList.toggle("popup_opened");
-  main.append(editProfilePopupElement);
+  popup.classList.add("popup_opened");
 
-  // описываем работу кнопок
   // кнопка закрытия модального окна
-  exitPopupButton.addEventListener("click", function () {
-    editProfilePopupElement.classList.toggle("popup_opened");
-  });
+  exitPopupButton.onclick =  function () {
+    popup.classList.remove("popup_opened");
+  };
 
-  // работа формы
-  formElement.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    nameElement.textContent = editProfilePopupElement.querySelector(
+  // кнопка сохранения
+  saveButton.onclick = function () {
+    nameElement.textContent = popup.querySelector(
       ".popup__input_el_name"
     ).value;
-    descriptionElement.textContent = editProfilePopupElement.querySelector(
+    descriptionElement.textContent = popup.querySelector(
       ".popup__input_el_description"
     ).value;
 
-    editProfilePopupElement.classList.toggle("popup_opened");
-  });
+    popup.classList.remove("popup_opened");
+  };
+
+  // работа формы
+  formElement.onsubmit = function (evt) {
+    evt.preventDefault();
+    nameElement.textContent = popup.querySelector(
+      ".popup__input_el_name"
+    ).value;
+    descriptionElement.textContent = popup.querySelector(
+      ".popup__input_el_description"
+    ).value;
+
+    popup.classList.remove("popup_opened");
+  };
 }
 
 // ------------------------ ДОБАВЛЕНИЕ КАРТОЧКИ --------------------------------
 
 function addNewCard() {
-  const popupTemplate = document.querySelector("#popup").content;
-  const addCardPopupElement = popupTemplate
-    .querySelector(".popup")
-    .cloneNode(true);
-  const exitPopupButton = addCardPopupElement.querySelector(
-    ".popup__exit-button"
-  );
-  const saveButton = addCardPopupElement.querySelector(".popup__save-button");
-  const formElement = addCardPopupElement.querySelector(
-    ".popup__form-container"
-  );
+  const popup = document.querySelector(".popup_type_new-card");
+  const exitPopupButton = popup.querySelector(".popup__exit-button");
+  const saveButton = popup.querySelector(".popup__save-button");
+  const formElement = popup.querySelector(".popup__form-container");
 
-  // настраиваем элементы модального окна
-  addCardPopupElement.querySelector(".popup__title").textContent =
-    "Новое место";
-  saveButton.textContent = "Создать";
-  let placeInput = addCardPopupElement.querySelector(
-    ".popup__form-container"
-  ).firstElementChild;
-  let linkInput = placeInput.nextElementSibling;
-
-  placeInput.classList.add("popup__input_el_place");
-  placeInput.setAttribute("name", "place");
-  placeInput.setAttribute("placeholder", "Название");
-
-  linkInput.classList.add("popup__input_el_link");
-  linkInput.setAttribute("name", "link");
-  linkInput.setAttribute("placeholder", "Ссылка на картинку");
+  // очищаем поля ввода
+  const placeInput = popup.querySelector(".popup__input_el_place-name");
+  const linkInput = popup.querySelector(".popup__input_el_link");
+  placeInput.value = "";
+  linkInput.value = "";
 
   // открываем окно
-  addCardPopupElement.classList.toggle("popup_opened");
-  main.append(addCardPopupElement);
+  popup.classList.add("popup_opened");
 
-  // описываем работу кнопок
   // кнопка закрытия модального окна
-  exitPopupButton.addEventListener("click", function () {
-    addCardPopupElement.classList.toggle("popup_opened");
-  });
+  exitPopupButton.onclick = function () {
+    popup.classList.remove("popup_opened");
+  };
+
+  // кнопка сохранения
+  saveButton.onclick = function () {
+    if (!placeInput.value || !linkInput.value) return;
+    createCard(placeInput.value, linkInput.value);
+    popup.classList.remove("popup_opened");
+  };
 
   // работа формы
-  formElement.addEventListener("submit", function (evt) {
+  formElement.onsubmit = function (evt) {
     evt.preventDefault();
     if (!placeInput.value || !linkInput.value) return;
     createCard(placeInput.value, linkInput.value);
-    addCardPopupElement.classList.toggle("popup_opened");
-  });
+    popup.classList.remove("popup_opened");
+  };
 }
 
 // --------------------------- СОЗДАНИЕ КАРТОЧКИ ------------------------------
@@ -162,6 +131,7 @@ function createCard(name, link) {
 
   cardElement.querySelector(".card__place-name").textContent = name;
 
+  // кнопка удаления
   cardElement
     .querySelector(".card__delete-button")
     .addEventListener("click", function (evt) {
@@ -175,7 +145,7 @@ function createCard(name, link) {
       const imageLink = evt.target.getAttribute("src");
       const imageName = evt.target.getAttribute("alt");
 
-      const watchCardPopupElement = document.querySelector(".popup");
+      const watchCardPopupElement = document.querySelector(".popup_type_view-card");
       const exitPopupButton = watchCardPopupElement.querySelector(
         ".popup__exit-button"
       );
@@ -194,6 +164,7 @@ function createCard(name, link) {
       });
     });
 
+  // лайк
   cardElement
     .querySelector(".card__like-button")
     .addEventListener("click", function () {
